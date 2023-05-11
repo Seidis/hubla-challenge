ce433abcea3ceb4c543c206105bab0f84713118e
Patrick Arruda <pluizarruda@gmail.com>
2023-05-11 04:42:51 -0300

    fix: type error on vendors endpoint

--- HublaBackEnd/api/vendors.py
+++ HublaBackEnd/api/vendors.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from database import get_db
 from api.auth import get_current_user
@@ -8,7 +9,7 @@ from schemas.vendors import BaseVendor
 router = APIRouter()
 
 
-@router.get("/", response_model=list[BaseVendor])
+@router.get("/", response_model=List[BaseVendor])
 async def get_vendors(db=Depends(get_db), user=Depends(get_current_user)):
     vendors = db.query(Vendors).filter(Vendors.user_id == user._data["localId"]).all()
     return vendors
